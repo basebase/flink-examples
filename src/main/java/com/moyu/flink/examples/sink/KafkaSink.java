8742b24ebbdb30a7eeaf75d602f5b7ad8d9602bd
李建峰 <base_2@163.com>
2021-06-02 22:00:40 +0800

    kafka 字符串sink

--- src/main/java/com/moyu/flink/examples/sink/KafkaSink.java
+++ src/main/java/com/moyu/flink/examples/sink/KafkaSink.java
@@ -1,7 +1,14 @@
 package com.moyu.flink.examples.sink;
 
+import com.moyu.flink.examples.model.Student;
+import org.apache.flink.api.common.serialization.SerializationSchema;
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.api.common.typeinfo.Types;
+import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.core.JsonProcessingException;
+import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer;
 
@@ -26,13 +33,51 @@ public class KafkaSink {
         /**
          *      写入数据到kafka使用FlinkKafkaProducer
          */
-        FlinkKafkaProducer<String> kafkaSink =
-                new FlinkKafkaProducer<>("kafka_sink", new SimpleStringSchema(), kafkaConf);
+//        FlinkKafkaProducer<String> kafkaSink =
+//                new FlinkKafkaProducer<>("kafka_sink", new SimpleStringSchema(), kafkaConf);
+//
+//        SingleOutputStreamOperator<String> strStream = socketSource.filter(value -> value != null && value.length() > 0).map(value -> {
+//            return value.replaceAll(",", "").toUpperCase();
+//        });
+//
+//        strStream.addSink(kafkaSink);
+
+
+        /***
+         *      将一个java bean序列化写入到kafka中, 但其实可以重写tostring方法利用SimpleStringSchema来实现比较方便
+         */
+        FlinkKafkaProducer<Student> studentKafkaSink =
+                new FlinkKafkaProducer<>("kafka_sink", new StudentSerializationSchema(), kafkaConf);
+
+        DataStream<Student> studentStream = socketSource.filter(value -> value != null && value.length() > 0).map(value -> {
+            String[] fields = value.split(",");
+            return new Student(Integer.parseInt(fields[0]), fields[1], Double.parseDouble(fields[2]));
+        }).returns(Types.POJO(Student.class));
+
+
+        studentStream.print();
 
         // 添加输出目标
-        socketSource.print();
-        socketSource.addSink(kafkaSink);
+        studentStream.addSink(studentKafkaSink);
 
         env.execute("Kafka Sink Test Job");
+    }
+
+    private static class StudentSerializationSchema implements SerializationSchema<Student> {
+        ObjectMapper mapper;
+
+        @Override
+        public byte[] serialize(Student element) {
+            byte[] b = null;
+            if (mapper == null) {
+                mapper = new ObjectMapper();
+            }
+            try {
+                b= mapper.writeValueAsBytes(element);
+            } catch (JsonProcessingException e) {
+                e.printStackTrace();
+            }
+            return b;
+        }
     }
 }
